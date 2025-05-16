f792a37aed9e9f011f4b574430d27286493abd96
joaoviitorsx <joaovitorssilva007@gmail.com>
2025-05-16 11:27:32 -0300

    melhorias em mensagem.py

--- utils/mensagem.py
+++ utils/mensagem.py
@@ -1,32 +1,30 @@
 from utils.icone import usar_icone
 from PySide6 import QtWidgets, QtGui, QtCore
 
-def mensagem_error(mensagem):
-    msg_erro = QtWidgets.QMessageBox()
-    msg_erro.setIcon(QtWidgets.QMessageBox.Critical)
-    msg_erro.setWindowTitle("Erro")
-    msg_erro.setText(mensagem)
-    msg_erro.setStyleSheet("background-color: #001F3F; color: #ffffff; font-size: 16px; font-weight: bold;")
-    usar_icone(msg_erro) 
-    msg_erro.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))    
-    msg_erro.exec()
+def _mostrar_mensagem(tipo: str, mensagem: str):
+    msg_box = QtWidgets.QMessageBox()
 
-def mensagem_sucesso(mensagem):
-    msg_sucesso = QtWidgets.QMessageBox()
-    msg_sucesso.setIcon(QtWidgets.QMessageBox.Information)
-    msg_sucesso.setWindowTitle("Sucesso")
-    msg_sucesso.setText(mensagem)
-    msg_sucesso.setStyleSheet("background-color: #001F3F; color: #ffffff; font-size: 16px; font-weight: bold;")
-    usar_icone(msg_sucesso) 
-    msg_sucesso.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))    
-    msg_sucesso.exec()
+    if tipo == 'erro':
+        msg_box.setIcon(QtWidgets.QMessageBox.Critical)
+        msg_box.setWindowTitle("Erro")
+    elif tipo == 'sucesso':
+        msg_box.setIcon(QtWidgets.QMessageBox.Information)
+        msg_box.setWindowTitle("Sucesso")
+    elif tipo == 'aviso':
+        msg_box.setIcon(QtWidgets.QMessageBox.Warning)
+        msg_box.setWindowTitle("Aviso")
+    
+    msg_box.setText(mensagem)
+    msg_box.setStyleSheet("background-color: #001F3F; color: #ffffff; font-size: 16px; font-weight: bold;")
+    usar_icone(msg_box)
+    msg_box.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+    msg_box.exec()
 
-def mensagem_aviso(mensagem):
-    msg_aviso = QtWidgets.QMessageBox()
-    msg_aviso.setIcon(QtWidgets.QMessageBox.Warning)
-    msg_aviso.setWindowTitle("Aviso")
-    msg_aviso.setText(mensagem)
-    msg_aviso.setStyleSheet("background-color: #001F3F; color: #ffffff; font-size: 16px; font-weight: bold;")
-    usar_icone(msg_aviso) 
-    msg_aviso.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))    
-    msg_aviso.exec()
+def mensagem_error(mensagem: str):
+    _mostrar_mensagem('erro', mensagem)
+
+def mensagem_sucesso(mensagem: str):
+    _mostrar_mensagem('sucesso', mensagem)
+
+def mensagem_aviso(mensagem: str):
+    _mostrar_mensagem('aviso', mensagem)
