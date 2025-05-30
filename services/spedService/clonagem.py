a9317fe31df566900092a92ae8110573a8604501
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-05-30 10:40:27 -0300

    add nome e cnpj c170_clone

--- services/spedService/clonagem.py
+++ services/spedService/clonagem.py
@@ -10,16 +10,18 @@ async def clonar_tabela_c170(empresa_id):
         cursor.execute("""
             INSERT IGNORE INTO c170_clone (
                 empresa_id, periodo, reg, num_item, cod_item, descr_compl,
-                qtd, unid, vl_item, vl_desc, cfop, cst, ncm, id_c100, filial,
-                ind_oper, cod_part, num_doc, chv_nfe, aliquota, resultado, chavefinal
+                qtd, unid, vl_item, vl_desc, cst, ncm, id_c100, filial,
+                ind_oper, cod_part, num_doc, chv_nfe, aliquota, resultado,
+                chavefinal, nome, cnpj
             )
             SELECT 
-                empresa_id, periodo, reg, num_item, cod_item, descr_compl,
-                qtd, unid, vl_item, vl_desc, cst_icms, cfop, NULL, cod_ncm, id_c100, filial,
-                ind_oper, cod_part, num_doc, chv_nfe, NULL, NULL,
-                CONCAT(cod_item, chv_nfe)
-            FROM c170nova
-            WHERE empresa_id = %s
+                n.empresa_id, n.periodo, n.reg, n.num_item, n.cod_item, n.descr_compl,
+                n.qtd, n.unid, n.vl_item, n.vl_desc, n.cst_icms, n.cod_ncm, n.id_c100, n.filial,
+                n.ind_oper, n.cod_part, n.num_doc, n.chv_nfe, NULL, NULL,
+                CONCAT(n.cod_item, n.chv_nfe),f.nome, f.cnpj
+            FROM c170nova n
+            LEFT JOIN `0150` f ON n.cod_part = f.cod_part AND n.empresa_id = f.empresa_id
+            WHERE n.empresa_id = %s
         """, (empresa_id,))
         
         conexao.commit()
