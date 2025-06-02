09e8efc707c3fa3348c77ce739a24f0f2e97c606
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-06-02 16:30:22 -0300

    limpeza com reset no id

--- services/spedService/limpeza.py
+++ services/spedService/limpeza.py
@@ -1,7 +1,5 @@
 from db.conexao import conectar_banco, fechar_banco
 
-from db.conexao import conectar_banco, fechar_banco
-
 def limpar_tabelas_temporarias(empresa_id):
     print("iniciando limpeza condicional")
     tabelas = ['`0000`', '`0150`', '`0200`', '`c100`', '`c170`', '`c170nova`']
@@ -15,11 +13,15 @@ def limpar_tabelas_temporarias(empresa_id):
             total = cursor.fetchone()[0]
             if total > 0:
                 cursor.execute(f"DELETE FROM {tabela} WHERE empresa_id = %s", (empresa_id,))
+                cursor.execute(f"SELECT COUNT(*) FROM {tabela}")
+                count_total = cursor.fetchone()[0]
+                if count_total == 0:
+                    cursor.execute(f"ALTER TABLE {tabela} AUTO_INCREMENT = 1")
                 houve_limpeza = True
 
         if houve_limpeza:
             conexao.commit()
-            print(f"[LIMPEZA] Dados temporários da empresa {empresa_id} removidos com sucesso.")
+            print(f"[LIMPEZA] Dados temporários da empresa {empresa_id} removidos com sucesso e AUTO_INCREMENT resetado.")
         else:
             print(f"[LIMPEZA] Nenhum dado temporário encontrado para a empresa {empresa_id}.")
 
