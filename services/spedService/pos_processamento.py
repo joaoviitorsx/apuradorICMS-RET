0f599f13e583f6a7803e8ab37930551b264219a0
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-06-02 14:59:13 -0300

    pos

--- services/spedService/pos_processamento.py
+++ services/spedService/pos_processamento.py
@@ -1,9 +1,12 @@
 from db.conexao import conectar_banco, fechar_banco
+from PySide6.QtWidgets import QMessageBox
 from services.fornecedorService import comparar_adicionar_atualizar_fornecedores
-from services.spedService.tributacao import criar_e_preencher_c170nova, atualizar_cadastro_tributacao
+from services.spedService.tributacao import criar_e_preencher_c170nova
 from services.spedService.atualizacoes import atualizar_ncm, atualizar_aliquota, atualizar_aliquota_simples,atualizar_resultado
-from services.spedService.clonagem import clonar_tabela_c170
-from services.spedService.verificacoes import verificar_e_abrir_popup_aliquota
+from services.spedService.clonagem import clonar_tabela_c170nova
+from services.spedService.verificacoes import verificar_e_abrir_popup_aliquota, preencherTributacao
+from services.spedService.relatorioPDF import gerar_pdf_historico, PDFPrompt
+from services.spedService.limpeza import limpar_tabelas_temporarias
 
 async def etapas_pos_processamento(empresa_id, progress_bar, janela_pai=None):
     print(f"[POS] Iniciando etapas de pós-processamento para empresa_id={empresa_id}...")
@@ -17,20 +20,16 @@ async def etapas_pos_processamento(empresa_id, progress_bar, janela_pai=None):
     print("[POS] Tabela c170nova criada e preenchida.")
 
     progress_bar.setValue(52)
-    await atualizar_cadastro_tributacao(empresa_id)
-    print("[POS] Tabela cadastro_tributacao atualizada com base em c170nova.")
+    await preencherTributacao(empresa_id, janela_pai)
+    print("[POS] Cadastro de tributação preenchido com base na tabela 0200.")
 
     progress_bar.setValue(54)
     await verificar_e_abrir_popup_aliquota(empresa_id, janela_pai)
     print("[POS] Popup de alíquotas exibido, se necessário.")
 
     progress_bar.setValue(60)
-    await clonar_tabela_c170(empresa_id)
+    await clonar_tabela_c170nova(empresa_id)
     print("[POS] Tabela c170_clone criada com sucesso.")
-
-    progress_bar.setValue(70)
-    await atualizar_ncm(empresa_id)
-    print("[POS] NCMs atualizados com base na tabela 0200.")
 
     await atualizar_aliquota(empresa_id)
     print("[POS] Alíquotas atualizadas na tabela c170_clone.")
@@ -55,3 +54,17 @@ async def etapas_pos_processamento(empresa_id, progress_bar, janela_pai=None):
 
     progress_bar.setValue(100)
     print("[POS] Pós-processamento concluído.")
+
+    if janela_pai:
+        prompt = PDFPrompt(empresa_id, janela_pai)
+        
+        prompt.exec()
+
+        if prompt.result() == QMessageBox.Yes:
+            nome_pdf = gerar_pdf_historico(empresa_id)
+            QMessageBox.information(janela_pai, "PDF gerado", f"O histórico foi salvo como:\n{nome_pdf}")
+            limpar_tabelas_temporarias(empresa_id)
+            print("[POS] Limpeza de tabelas temporárias concluída.")
+
+
+
