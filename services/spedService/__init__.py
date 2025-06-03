7b8704b35f127ed6e7d7887ce85795e41b438586
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-06-03 15:13:43 -0300

    init

--- services/spedService/__init__.py
+++ services/spedService/__init__.py
@@ -2,7 +2,7 @@ from .carregamento import processar_sped
 from .salvamento import salvar_no_banco_em_lote
 from .pos_processamento import etapas_pos_processamento
 from .tributacao import criar_e_preencher_c170nova
-from .atualizacoes import atualizar_ncm, atualizar_aliquota, atualizar_aliquota_simples, atualizar_resultado
+from .atualizacoes import atualizar_aliquota, atualizar_aliquota_simples, atualizar_resultado
 from .verificacoes import verificar_e_abrir_popup_aliquota, preencherTributacao, sinal_popup
 
 
