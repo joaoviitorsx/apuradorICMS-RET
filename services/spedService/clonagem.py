f3536dff68037a7be8a8e5ed70e348c74959238e
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-06-05 11:36:17 -0300

    clonagem

--- services/spedService/clonagem.py
+++ services/spedService/clonagem.py
@@ -39,21 +39,3 @@ async def clonar_tabela_c170nova(empresa_id):
         cursor.close()
         fechar_banco(conexao)
         print("[FIM] Clonagem finalizada.")
-
-
-
-
-
-#precisa adicionar aliquota e resultado na tabela c170_clone que seria a clonagem da tabela c170nova,
-#fazendo um cadastro de produtos com base nos produtos na tabela 0200
-#precisamos verificar os cod_item que serao dos dos produtos no 0200 e fazendo assim uma verificação se o produto ja existe no banco
-#cadastro_tributacao, se nao existir vamos inseri-lo, no entanto, não vai estar presente as aliquotas
-# e assim vai abrir o popup para o usuario preencher as aliquotas, como pode ter varios produtos igual, mas com chaves diferentes,
-# so vamos filtrar para ser chamado o produto uma vez e assim o usuario preenche as aliquotas de todos os produtos iguais
-
-#quando os dados sao preenchidos na c170nova ele coleta dados da c100 e 0200 para serem corretamente preenchidos,
-#portanto o proximo passo seria a clonagem de c170nova para inserimos as aliquotas e resultados deste produtos que passaram pelo o filtro
-# no entanto, apos fazer a clonagem e antes de passarem pela atualização das aliquotas ou pelo o codigo em atualizacoes.py
-# precisamos aplicar e ve os produtos de 0200 que nao estao presente em cadastro_Tributacao e inserir eles, pois verificando no banco
-# indentifiquei que na tabela c170nova temos itens que nao estao presentes e entao para finalizarmos o processamento precisamos
-# alimentar a base de cadastro_tributacao com os itens que estao presentes na c170nova
