8aa9cc49ddbde4fc27c5a619adef4926b0bc344a
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-06-05 11:37:52 -0300

    icone

--- utils/icone.py
+++ utils/icone.py
@@ -6,7 +6,6 @@ def usar_icone(janela):
     caminho_png = os.path.join("images", "icone.png")
     caminho_ico = os.path.join("images", "icone.ico")
 
-    # Usa .ico no Windows e .png no Linux/macOS
     if sys.platform == "win32" and os.path.exists(caminho_ico):
         janela.setWindowIcon(QtGui.QIcon(caminho_ico))
     elif os.path.exists(caminho_png):
